4b474488be593c61b094a8dcffaae1fc80263c05
Mary <bmacha23@gmail.com>
2019-04-14 15:53:21 +0300

    Добавлено восьмое задание

---
+++ 8/index.php
@@ -0,0 +1,65 @@
+
+<?php
+include "index.html";
+include "helper.php";
+if (!isset($_REQUEST['month'])) $month = "now";
+else $month = $_REQUEST['month'];
+
+$fileArr = file("month.txt");
+$fileArr = explode(",", $fileArr[0]);
+
+if ($month == "now") {
+    $dateTime = new DateTime();
+    $dateTime->setDate(date("o", $dateTime->getTimestamp()),
+        date("n", $dateTime->getTimestamp()),1);}
+else $dateTime = mySetDate($fileArr,$month);
+echo "<b>".date("l", $dateTime->getTimestamp())."</b>";
+$num = 0;
+$wed = 0;
+
+$begin = $dateTime;
+$year = date("o", $dateTime->getTimestamp());
+$m = date("n", $dateTime->getTimestamp());
+$d = date("t", $dateTime->getTimestamp());
+$end = new DateTime();
+$end->setDate($year,$m,$d);
+$end = $end->modify( '+1 day' );
+
+$interval = new DateInterval('P1D');
+$daterange = new DatePeriod($begin, $interval ,$end);
+
+$non = addEl($dateTime);
+
+?>
+
+
+    <h3><?php echo date("F", $dateTime->getTimestamp()) ?></h3>
+    <table style="">
+        <tr >
+            <td>пн</td><td>вт</td><td>ср</td><td>чт</td><td>пт</td><td>сб</td><td>вс</td>
+        </tr>
+        <?php
+        foreach ($non as $item){
+            if (!($num%7)) echo "<tr>";
+            if ($num==5||$num==6||$num==12||$num==13||$num==19||$num==20||$num==26||$num==27||$num=33||$num==34){
+                echo "<td style='color: red'> </td>";
+            }
+            else
+                echo "<td> </td>";
+            $num ++;
+            if (!($num%7)) echo "</tr>";
+        }
+        foreach($daterange as $date){
+            if (!($num%7)) echo "<tr>";
+            if ($num==5||$num==6||$num==12||$num==13||$num==19||$num==20||$num==26||$num==27||$num=33||$num==34){
+                echo "<td style='color: red'>";
+            }
+            else
+                echo "<td>";
+            echo date("j", $date->getTimestamp())."</td>";
+            $num ++;
+            if (!($num%7)) echo "</tr>";
+        }
+
+        ?>
+    </table>
